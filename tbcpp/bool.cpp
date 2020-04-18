f81c72c9e41c7c3e253e69073eda1cef4154c270
cskrub33 <cskrub33@naver.com>
2020-04-18 12:48:17 +0900

    Add project files.

---
+++ tbcpp/bool.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+
+bool isEqual(int a, int b)
+{
+	bool result = (a == b);
+	return result;
+
+
+}
+
+int main()
+{
+
+	char c1(65);
+
+	//cout << sizeof(unsigned char) << endl;
+	//cout << (int)std::numeric_limits<unsigned char>::max() << endl;
+	//cout << (int)std::numeric_limits<unsigned char>::lowest() << endl;
+
+	cout << int('\n') << endl;
+	cout << "this is first \navenger!!" << std::flush;
+
+
+
+
+	return 0;
+
+}
---
+++ tbcpp/tbcpp.cpp
@@ -0,0 +1,64 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <limits>
+//#include <cmath>
+//
+//
+//
+//using namespace std;
+//
+//int main()
+//{
+//	double zero = 0.0;
+//	double posinf = 5.0 / zero;
+//	double neginf = -5.0 / zero;
+//	double nan = zero / zero;
+//
+//	cout << posinf<< "   "  << std::isinf(posinf) << endl;
+//	cout << neginf << "   " << std::isnan(neginf) << endl;
+//	cout << nan << "    " << std::isnan(nan)<<"nan value is : " <<nan<< endl;
+//	cout << 1.0 << "    " << std::isnan(1.0) << endl;
+
+	//double d1(1.0);
+	//double d2(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
+
+	//cout << setprecision(17);
+
+	//cout << d1 << endl;
+	//cout << d2 << endl;
+
+	
+
+
+	//double d(0.1);
+
+	//cout << d << endl;
+	//cout << std::setprecision(17);
+	//cout << d << endl;
+
+
+
+	//float f(123456789.0f); //10 significat digits
+	//cout << std::setprecision(16);
+	//cout << f << endl;
+
+	//float f(3.141592f);
+	//double d(3.141592);
+	//long double ld(3.141592);
+
+	//cout << numeric_limits<float>::lowest() << endl;
+	//cout << numeric_limits<double>::lowest() << endl;
+	//cout << numeric_limits<long double>::lowest() << endl;
+
+	//cout << 3.14 << endl;
+	//cout << 31.4e-1 << endl;
+	//cout << 31.4e-2 << endl;
+	//cout << 31.4e1 << endl;
+	//cout << 31.4e2 << endl;
+//
+//	return 0;
+//
+//
+//
+//
+//}
